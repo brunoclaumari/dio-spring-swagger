8e87e4931168e0ef3f50bf1e94b7570216e0a463
brunoclaumari <brunoclaumari@gmail.com>
2023-11-17 23:10:54 -0300

    Example application Spring Boot using Swagger for API documentation

---
+++ src/main/java/com/swagger/demo/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.swagger.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		
+		return "Welcome to my Spring Boot Web API";
+	}
+
+}
---
+++ src/main/java/com/swagger/demo/handler/CampoObrigatorioException.java
@@ -0,0 +1,13 @@
+package com.swagger.demo.handler;
+
+public class CampoObrigatorioException extends BusinessException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public CampoObrigatorioException(String campo) {
+		super("O campo %s é obrigatório", campo);		
+	}
+
+	
+
+}
